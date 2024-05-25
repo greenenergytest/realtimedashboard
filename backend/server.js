c403fc810f01fb9c7c7afc78eda749d78b965f26
Mitema <mitema.emmanuel@gmail.com>
2024-05-25 04:22:20 +0100

    correct port and and correctly read environment variables

--- backend/server.js
+++ backend/server.js
@@ -4,32 +4,19 @@ const userRoutes = require('./routes/api/users');
 const profileRoutes = require('./routes/api/profile');
 const fileRoutes = require('./routes/api/file');
 const graphDataRoutes = require('./routes/api/graphData');
-
 const cors = require('cors');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
-//const crypto = require('crypto');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// const generateJWTSecret = () => {
-//   return crypto.randomBytes(32).toString('hex');
-// };
-
-// const JWT_SECRET = generateJWTSecret();
-
-// console.log('JWT Secret:', JWT_SECRET);
-
-//allow requests from any origin
 app.use(cors());
-
 const DB_URI = process.env.MONGODB_URI;
-// make the connection to db
-// on success return successful string
 console.log(DB_URI);
 app.use(bodyParser.json());
+
 mongoose
   .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
@@ -42,8 +29,5 @@ app.use('/', fileRoutes);
 app.use('/', graphDataRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is listening at htpp://localhost:${port}`);
+  console.log(`Server is listening at http://localhost:${port}`);
 });
-//  => {
-//     res.send(userRoutes)
-// })
