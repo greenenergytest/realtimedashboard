f1f49601d51ea92b1846f6cb16cf5310988b3bf1
Mitema <mitema.emmanuel@gmail.com>
2024-05-25 17:42:14 +0100

    add coreutils to aptfile and cat to .sh file

--- .profile.d/WEB_CONCURRENCY.sh
+++ .profile.d/WEB_CONCURRENCY.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-
-if [ -n "$WEB_MEMORY" ]; then
-  WEB_CONCURRENCY=$(echo "scale=2; $WEB_MEMORY / 512" | bc)
-  export WEB_CONCURRENCY=${WEB_CONCURRENCY%.*}
-fi
+# Get total memory in MB
+MEMORY=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+# Calculate concurrency
+WEB_CONCURRENCY=$((MEMORY / 1048576 / 512))
+# Ensure at least 1 concurrency
+WEB_CONCURRENCY=${WEB_CONCURRENCY:-1}
+# Export the variable
+export WEB_CONCURRENCY
