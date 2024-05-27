fad4fc96ef0c79598e6ed71734dd030dee9bb581
Mitema <mitema.emmanuel@gmail.com>
2024-05-27 16:21:17 +0100

    Fix startup issues

--- .profile.d/WEB_CONCURRENCY.sh
+++ .profile.d/WEB_CONCURRENCY.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 # Get total memory in MB
-MEMORY=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+if [ -r /sys/fs/cgroup/memory/memory.limit_in_bytes ]; then
+  MEMORY=$(< /sys/fs/cgroup/memory/memory.limit_in_bytes)
+else
+  echo "Error: Unable to read /sys/fs/cgroup/memory/memory.limit_in_bytes"
+  exit 1
+fi
+
 # Calculate concurrency
 WEB_CONCURRENCY=$((MEMORY / 1048576 / 512))
+
 # Ensure at least 1 concurrency
 WEB_CONCURRENCY=${WEB_CONCURRENCY:-1}
+
 # Export the variable
 export WEB_CONCURRENCY
+
