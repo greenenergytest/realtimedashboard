c3f88d7035daf71c71fba880eb6baa053517f1a3
Mitema <mitema.emmanuel@gmail.com>
2024-05-28 16:24:23 +0100

    fixed web_concurrency.sh file

--- .profile.d/WEB_CONCURRENCY.sh
+++ .profile.d/WEB_CONCURRENCY.sh
@@ -1,18 +1,11 @@
-#!/bin/sh
-# Get total memory in MB
-if [ -r /sys/fs/cgroup/memory/memory.limit_in_bytes ]; then
-  MEMORY=$(< /sys/fs/cgroup/memory/memory.limit_in_bytes)
+# Ensure cat and correct arithmetic operations are available
+if [ -f /sys/fs/cgroup/memory/memory.limit_in_bytes ]; then
+  MEM_LIMIT=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+  if [ "$MEM_LIMIT" -lt 536870912 ]; then
+    export WEB_CONCURRENCY=1
+  else
+    export WEB_CONCURRENCY=$(($MEM_LIMIT / 1048576000))
+  fi
 else
-  echo "Error: Unable to read /sys/fs/cgroup/memory/memory.limit_in_bytes"
-  exit 1
+  export WEB_CONCURRENCY=1
 fi
-
-# Calculate concurrency
-WEB_CONCURRENCY=$((MEMORY / 1048576 / 512))
-
-# Ensure at least 1 concurrency
-WEB_CONCURRENCY=${WEB_CONCURRENCY:-1}
-
-# Export the variable
-export WEB_CONCURRENCY
-
