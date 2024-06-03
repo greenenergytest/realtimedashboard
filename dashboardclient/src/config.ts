dd2088895f22b0304a4fe8a064ecc0237c116f56
Mitema <mitema.emmanuel@gmail.com>
2024-06-03 10:48:10 +0100

    added config.ts to keep url in one place and changed the baseUrl to point to herokui

---
+++ dashboardclient/src/config.ts
@@ -0,0 +1,5 @@
+const config = {
+  apiBaseUrl: 'https://dashboardbackend-6b8a4532b2b2.herokuapp.com/',
+};
+
+export default config;
--- dashboardclient/src/features/auth/authService.ts
+++ dashboardclient/src/features/auth/authService.ts
@@ -1,16 +1,21 @@
 import axios from 'axios';
+import config from '../../config';
 
-const API_URL = 'http://localhost:3000';
+//const API_URL = 'http://localhost:3000';
 
 //Register user
 const register = async (userData: any) => {
   console.log('in register service');
   try {
-    const response = await axios.post(API_URL + '/register', userData, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    const response = await axios.post(
+      config.apiBaseUrl + '/register',
+      userData,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
 
     if ((response.status = 200 && response.data)) {
       console.log('storing user to local host');
@@ -30,7 +35,7 @@ const login = async (userData: any) => {
   console.log('in login service');
   console.log(userData);
   try {
-    const response = await axios.post(API_URL + '/login', userData, {
+    const response = await axios.post(config.apiBaseUrl + '/login', userData, {
       headers: {
         'Content-Type': 'application/json',
       },
