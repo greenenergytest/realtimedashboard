64dd05e7935188131f2532afeac271ab44d4b78e
Mitema <mitema.emmanuel@gmail.com>
2024-07-08 15:08:57 +0100

    extended width of react plotly graph, different scales for primary and secondary graph

--- dashboardclient/src/components/PlotGraph.tsx
+++ dashboardclient/src/components/PlotGraph.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Plot from 'react-plotly.js';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
+import 'react-resizable/css/styles.css';
 
 interface PlotGraphProps {
   xData: any[];
   yPrimaryData: any[];
-  ySecondaryData: any[];
+  ySecondaryData?: any[];
+}
+
+// Define the shape of the resize data
+interface ResizeData {
+  size: {
+    width: number;
+    height: number;
+  };
 }
 
 const PlotGraph: React.FC<PlotGraphProps> = ({
@@ -12,13 +22,15 @@ const PlotGraph: React.FC<PlotGraphProps> = ({
   yPrimaryData,
   ySecondaryData,
 }) => {
+  const [dimensions, setDimensions] = useState({
+    width: Number('100%'),
+    height: 400,
+  });
   if (
     !xData ||
     !yPrimaryData ||
-    !ySecondaryData ||
     xData.length === 0 ||
-    yPrimaryData.length === 0 ||
-    ySecondaryData.length === 0
+    yPrimaryData.length === 0
   ) {
     return <div>No data to display</div>;
   }
@@ -26,7 +38,7 @@ const PlotGraph: React.FC<PlotGraphProps> = ({
   //Extract header labels for yPrimaryData and ySecondaryData
 
   const yPrimaryHeaders = yPrimaryData[0];
-  const ySecondaryHeaders = ySecondaryData[0];
+  const ySecondaryHeaders = ySecondaryData ? ySecondaryData[0] : [];
 
   //Prepare traces for primary y-axis
   const primaryTraces = yPrimaryHeaders.map((header: any, headerIndex: any) => {
@@ -44,7 +56,7 @@ const PlotGraph: React.FC<PlotGraphProps> = ({
   //prepare traces for secondary y-axis
   const secondaryTraces = ySecondaryHeaders.map(
     (header: any, headerIndex: any) => {
-      const yValues = ySecondaryData.slice(1).map((row) => row[headerIndex]);
+      const yValues = ySecondaryData?.slice(1).map((row) => row[headerIndex]);
       return {
         x: xData,
         y: yValues,
@@ -61,17 +73,68 @@ const PlotGraph: React.FC<PlotGraphProps> = ({
       title: 'Date',
     },
     yaxis: {
-      title: 'Primary Y axis',
+      title: yPrimaryHeaders.join(' / '),
       automargin: true,
     },
     yaxis2: {
-      title: 'Secondary Y axis',
+      title: ySecondaryHeaders.join(' / '),
       overlaying: 'y',
       side: 'right',
       automargin: true,
     },
+    legend: {
+      orientation: 'h',
+      y: -0.3,
+    },
   };
 
-  return <Plot data={[...primaryTraces, ...secondaryTraces]} layout={layout} />;
+  const config = {
+    responsive: true,
+  };
+
+  const handleResize: ResizableBoxProps['onResize'] = (
+    event: React.SyntheticEvent<Element>,
+    data: ResizeData
+  ) => {
+    console.log(event);
+    setDimensions({
+      width: data.size.width,
+      height: data.size.height,
+    });
+  };
+
+  return (
+    <>
+      {primaryTraces && ySecondaryData && ySecondaryData.length > 0 && (
+        <ResizableBox
+          width={dimensions.width}
+          height={dimensions.height}
+          minConstraints={[400, 300]}
+          maxConstraints={[800, 600]}
+          onResize={handleResize}
+          resizeHandles={['se']}
+          style={{ position: 'relative' }}
+        >
+          <Plot
+            data={[...primaryTraces, ...secondaryTraces]}
+            layout={layout}
+            useResizeHandler={true}
+            style={{ width: '100%', height: '100%' }}
+            config={config}
+          />
+        </ResizableBox>
+      )}
+
+      {primaryTraces && !ySecondaryHeaders && ySecondaryHeaders.length == 0 && (
+        <Plot
+          data={[...primaryTraces]}
+          layout={layout}
+          useResizeHandler={true}
+          style={{ width: '100%', height: '100%' }}
+          config={config}
+        />
+      )}
+    </>
+  );
 };
 export default PlotGraph;
