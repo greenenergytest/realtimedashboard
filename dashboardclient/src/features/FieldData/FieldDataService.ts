e244920fafe57fafbf714c0afdca49d6c274e3b8
greenenergytest <greenenergytest2024@gmail.com>
2024-10-22 16:16:52 +0100

    changes to display fieldview summary graph and card

--- dashboardclient/src/features/FieldData/FieldDataService.ts
+++ dashboardclient/src/features/FieldData/FieldDataService.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import config from '../../config';
 
-export const fetchFieldData = async (sheetName: string) => {
+export const fetchFieldData = async (sheetName: string, fileName: string) => {
   try {
     const response = await axios.post(config.apiBaseUrl + '/getFieldData', {
       sheetName,
+      fileName,
     });
     return response.data;
   } catch (error) {
