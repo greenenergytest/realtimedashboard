3f555c6a99e1b78f9310bebfae5d982bb72264b1
greenenergytest <greenenergytest2024@gmail.com>
2026-01-28 13:30:54 +0100

    added spinner to documents

--- dashboardclient/src/pages/documents.tsx
+++ dashboardclient/src/pages/documents.tsx
@@ -1,141 +1,86 @@
 import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import * as fs from 'fs';
-import * as path from 'node:path';
-import { fileURLToPath } from 'url';
-import { useLocation } from 'react-router-dom';
-import { getAllDocuments } from '../features/documents/documentsSlice';
-import { deleteDocument } from '../features/documents/documentsSlice';
-import { ListGroup, Badge, Button } from 'react-bootstrap';
-import { Download } from 'react-bootstrap-icons';
+import { useDispatch } from 'react-redux';
+import {
+  getAllDocuments,
+  deleteDocument,
+} from '../features/documents/documentsSlice';
+import { ListGroup, Badge, Button, Spinner } from 'react-bootstrap';
+import { Download, Trash } from 'react-bootstrap-icons';
 import config from '../config';
-import { Trash } from 'react-bootstrap-icons';
-//read the files from the uploads and store in an array
-//loop through the files
-//diplay the files on the page
-
-//const path = require('path');
-//const fs = require('fs');
-
-/*const location = useLocation();
-const __filename = fileURLToPath(location.pathname);
-
-console.log(__filename);
-
-//console.log(__filename);
-
-let __dirname = path.dirname(__filename);
-let uploadsDir = path.join(__dirname, 'uploads');
-
-const getDocumentLink = () => {
-  fetch(`http://192.168.1.232:5000/api/${uploadsDir}`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-}; */
-
-//get the name of the doc
-// and match it to that in the folder
 
 const DocumentFieldView = () => {
-  const dispatch = useDispatch();
-  const [documents, setDocuments] = useState<any>({
+  const dispatch = useDispatch<any>();
+
+  const [documents, setDocuments] = useState<{
+    payload: { files: Record<string, string> };
+  }>({
     payload: { files: {} },
   });
 
-  // const list_colors = ['primary', 'secondary', 'success', 'danger'];
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const load_documents = async () => {
-      const docs = await dispatch(getAllDocuments() as any);
-
+    const loadDocuments = async () => {
+      setLoading(true);
+      const docs = await dispatch(getAllDocuments());
       setDocuments(docs);
-      // console.log(documents);
-      //console.log('in load documents');
+      setLoading(false);
     };
 
-    load_documents();
-  }, []);
+    loadDocuments();
+  }, [dispatch]);
 
-  const downloadDocuments = async (doc: any, fileName: string) => {
-    // if (doc instanceof Blob) {
-    //192.168.1.172:5000
-    // const link = document.createElement('a');
-    // link.href = `http://192.168.1.172:5000/uploads/${fileName}`;
-    // link.download = fileName;
-    // link.click();
+  const downloadDocument = (fileName: string) => {
     const link = document.createElement('a');
     link.href = `${config.apiBaseUrl}/download/${fileName}`;
-    //link.href = ;
-
     link.click();
   };
 
-  const handleDelete = async (value: unknown) => {
-    console.log(value);
-    // pass filename to backend service
-    // delete the file
-    // refresh the list in useEffect
-    await dispatch(deleteDocument(String(value)) as any);
-    const docs = await dispatch(getAllDocuments() as any);
+  const handleDelete = async (fileName: string) => {
+    setLoading(true);
+    await dispatch(deleteDocument(fileName));
+    const docs = await dispatch(getAllDocuments());
     setDocuments(docs);
+    setLoading(false);
   };
-  // getDocumentLink();
-  //   fs.readdir(
-  //     uploadsDir,
-  //     (err: NodeJS.ErrnoException | null, files: string[] | undefined) => {
-  //       if (err) {
-  //         throw err;
-  //       }
-  //       console.log(files);
-  //     }
-  //   );
 
   return (
     <div>
-      {/* <h3 style={{ color: 'black' }}> Documents </h3> */}
-      <ListGroup style={{ display: 'flex', gap: '8px' }}>
-        {Object.entries(documents.payload.files).map(([key, value], index) => {
-          {
-            console.log(value);
-          }
-          return (
-            <ListGroup key={key}>
-              <ListGroup.Item
-                className='d-flex justify-content-between align-items-start'
-                style={{ backgroundColor: '#214035' }}
-
-                //  className="d-flex justify-content-between align-items-start"
+      {loading ? (
+        <Spinner animation='border' role='status'>
+          <span className='visually-hidden'>Loading...</span>
+        </Spinner>
+      ) : (
+        <ListGroup>
+          {Object.entries(documents.payload.files).map(([key, value]) => (
+            <ListGroup.Item
+              key={key}
+              className='d-flex justify-content-between align-items-center'
+              style={{ backgroundColor: '#214035', color: 'white' }}
+            >
+              <Button
+                variant='light'
+                className='rounded-circle'
+                onClick={() => handleDelete(value)}
               >
+                <Trash size={16} />
+              </Button>
+
+              <span>{value.substring(value.lastIndexOf('-') + 1)}</span>
+
+              <Badge bg='light'>
                 <Button
                   variant='light'
-                  className='rounded-circle d-flex align-items-center justify-content-center'
-                  style={{ width: '36px', height: '36px' }}
-                  onClick={() => handleDelete(value)}
+                  size='sm'
+                  onClick={() => downloadDocument(value)}
                 >
-                  <Trash size={16} />
+                  <Download size={14} />
                 </Button>
-                <ListGroup.Item variant='success'>
-                  {value
-                    ? (value as string).substring(
-                        (value as string).lastIndexOf('-') + 1,
-                      )
-                    : String(value)}
-                </ListGroup.Item>
-                <Badge bg='#214035' pill>
-                  <Button
-                    variant='light'
-                    size='sm'
-                    className='p-1 d-flex align-items-center'
-                    onClick={() => downloadDocuments(value, String(value))}
-                  >
-                    <Download size={14} />
-                  </Button>
-                </Badge>
-              </ListGroup.Item>
-            </ListGroup>
-          );
-        })}
-      </ListGroup>
+              </Badge>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      )}
     </div>
   );
 };
